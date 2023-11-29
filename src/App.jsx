1f227173d6137b66e70f1590146f8116a1e63959
Anudeep Chowdary <sirigirianudeep348@gmail.com>
2023-11-29 12:43:30 +0530

    Implimented fake authentication to the application in 3 steps

--- src/App.jsx
+++ src/App.jsx
@@ -11,28 +11,39 @@ import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
 import { CitesProvider } from "./Context/CitesContext";
+import { AuthProvider } from "./Context/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
   return (
     <div>
-      <CitesProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route index element={<Homepage />} />
-            <Route path="Product" element={<Product />} />
-            <Route path="Pricing" element={<Pricing />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
-              <Route index element={<Navigate replace to="cites" />} />
-              <Route path="cites" element={<CityList />} />
-              <Route path="cites/:id" element={<City />} />
-              <Route path="countries" element={<CountryList />} />
-              <Route path="form" element={<Form />} />
-            </Route>
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </CitesProvider>
+      <AuthProvider>
+        <CitesProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route index element={<Homepage />} />
+              <Route path="Product" element={<Product />} />
+              <Route path="Pricing" element={<Pricing />} />
+              <Route path="/login" element={<Login />} />
+              <Route
+                path="app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<Navigate replace to="cites" />} />
+                <Route path="cites" element={<CityList />} />
+                <Route path="cites/:id" element={<City />} />
+                <Route path="countries" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </CitesProvider>
+      </AuthProvider>
     </div>
   );
 }
